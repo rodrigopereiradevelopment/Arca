9d3d96b7c066a9bffbe03e87f33f829df7866d8a
rodrigodevelopmmt <rodrigopereira.development@gmail.com>
2025-10-04 18:20:33 -0300

    update

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.querySelector('.search-btn').onclick = function () {
+  alert('Buscar: ' + document.querySelector('.search-bar input').value);
+};
+document.querySelector('.menu-btn').onclick = function () {
+  alert('Abrir menu...');
+};
